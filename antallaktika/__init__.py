c5d901484ee21304e5d3424a71d074e59f067e88
kosazna <kaznavouridis@gmail.com>
2021-01-08 23:28:25 +0200

    (L) updated scripts

--- antallaktika/__init__.py
+++ antallaktika/__init__.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from atcrawl.antallaktika.crawler import *
+
+file = Path.home().joinpath("Documents\\.config.txt")
+
+if file.exists():
+    counter = int(file.read_text())
+else:
+    with open(file, 'w') as f:
+        f.write("1")
+    counter = 1
+
+
+def log(what):
+    with open(file, 'w') as tf:
+        tf.write(str(what))
--- main.py
+++ main.py
@@ -5,36 +5,40 @@ from atcrawl.antallaktika import *
 
 print(f"atCrawl utilities\n")
 
-url = input("\nGive URL:\n")
-is_valid_url = requests.get(url).status_code
-ao = None
+if counter < 20:
+    url = input("\nGive URL:\n")
+    is_valid_url = requests.get(url).status_code
+    ao = None
 
-try:
-    if is_valid_url:
-        ao = PageBlock(url)
-        ao.launch('Chrome', 'chromedriver.exe')
+    try:
+        if is_valid_url:
+            ao = PageBlock(url)
+            ao.launch('Chrome', 'chromedriver.exe')
 
-        print("\n\nCrawler is collecting the data...\n\n")
+            print("\n\nCrawler is collecting the data...\n\n")
 
-        ao.collect()
-    else:
-        print("\nNot a valid URL\n")
-except KeyboardInterrupt:
-    print("\nProcess cancelled by user\n")
-finally:
-    export = input("\nDo you want to export the collected data?\n[y/n]").lower()
-    if export == 'y':
-        _filename = input("\nEnter filename:\n")
-        _folder = input("\nDestination folder:\n")
-
-        if _folder == '':
-            folder = Path().cwd()
+            ao.collect()
         else:
-            folder = _folder
+            print("\nNot a valid URL\n")
+    except KeyboardInterrupt:
+        print("\nProcess cancelled by user\n")
+    finally:
+        log(counter + 1)
+        export = input("\nExport the collected data?\n[y/n]").lower()
+        if export == 'y':
+            _filename = input("\nEnter filename:\n")
+            _folder = input("\nDestination folder:\n")
 
-        if _filename == '':
-            filename = 'Collected Data'
-        else:
-            filename = _filename
+            if _folder == '':
+                folder = Path().cwd()
+            else:
+                folder = _folder
 
-        ao.export(filename, folder)
+            if _filename == '':
+                filename = 'Collected Data'
+            else:
+                filename = _filename
+
+            ao.export(filename, folder)
+else:
+    print("[Access denied]")

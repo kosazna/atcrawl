bfe29c34dcb0b6728110ade907e662a6282b1184
kosazna <kaznavouridis@gmail.com>
2021-01-22 20:50:57 +0200

    (D) updated scripts

--- utilities/paths.py
+++ utilities/paths.py
@@ -6,9 +6,11 @@ from pathlib import Path
 class Paths:
     def __init__(self):
         self._userhome = Path.home()
+        self._atcrawl = self._userhome.joinpath(".atcrawl")
         self._chrome = self._userhome.joinpath("chromedriver.exe")
         self._firefox = self._userhome.joinpath("geckodriver.exe")
         self._cwd = Path.cwd()
+        self.check_app_folder()
 
     def get_userhome(self) -> str:
         return str(self._userhome)
@@ -21,6 +23,10 @@ class Paths:
 
     def get_cwd(self):
         return self._cwd
+
+    def check_app_folder(self):
+        if not self._atcrawl.exists():
+            self._atcrawl.mkdir(parents=True, exist_ok=True)
 
 
 paths = Paths()

5795b62a7b93d2164a4904e043e8a78bb734390f
kosazna <kaznavouridis@gmail.com>
2021-02-03 13:15:54 +0200

    (D) updated scripts

--- merge.py
+++ merge.py
@@ -1,34 +1,42 @@
 import pandas as pd
 from pathlib import Path
 
-cwd = Path(input("Φάκελος αρχείων:\n"))
-files = list(cwd.glob('*.xlsx'))
+while True:
+    cwd = Path(input("Φάκελος αρχείων:\n"))
+    files = list(cwd.glob('*.xlsx'))
 
-if files:
-    print(f"\nΕντοπίστηκαν {len(files)} αρχεία.\n")
-    print("Γίνεται ένωση...\n")
-    to_concat = []
+    if files:
+        print(f"\nΕντοπίστηκαν {len(files)} αρχεία.\n")
+        print("Γίνεται ένωση...\n")
+        to_concat = []
 
-    for file in files:
-        df = pd.read_excel(file)
-        to_concat.append(df)
+        for file in files:
+            df = pd.read_excel(file)
+            to_concat.append(df)
 
-    merged_df = pd.concat(to_concat)
+        merged_df = pd.concat(to_concat)
 
-    to_add_col = input("Θες να προσθέσεις κάποια στήλη [y/n]\n").upper()
+        to_add_col = input("Θες να προσθέσεις κάποια στήλη? [y/n]\n").upper()
 
-    if to_add_col == 'Y':
-        col_name = input("Όνομα στήλης:\n")
-        col_value = input("Τιμή στήλης:\n")
+        if to_add_col == 'Y':
+            col_name = input("Όνομα στήλης:\n")
+            col_value = input("Τιμή στήλης:\n")
 
-        merged_df[col_name] = col_value
+            merged_df[col_name] = col_value
 
-    save_base_name = input("Όνομα αποθήκευσης αρχείου\n")
-    save_suffix = ".xlsx"
-    save_name = f"{save_base_name}{save_suffix}"
-    save_filepath = cwd.joinpath(save_name)
+        save_base_name = input("Όνομα αποθήκευσης αρχείου\n")
+        save_suffix = ".xlsx"
+        save_name = f"{save_base_name}{save_suffix}"
+        save_filepath = cwd.joinpath(save_name)
 
-    merged_df.to_excel(save_filepath, index=False)
-    print(f"\nΤο αρχείο δημιουργήθηκε στη τοποθεσία:\n -> {str(save_filepath)}")
-else:
-    print("\nΔεν εντοπίστηκαν αρχεία '.xlsx'\n")
+        merged_df.to_excel(save_filepath, index=False)
+        print(f"\nΤο αρχείο δημιουργήθηκε:\n -> {str(save_filepath)}")
+    else:
+        print("\nΔεν εντοπίστηκαν αρχεία '.xlsx'\n")
+
+    _status = input("\n\nΘες να ενώσεις και άλλα αρχεία? [y/n]\n").upper()
+
+    if _status == 'Y':
+        continue
+    else:
+        break

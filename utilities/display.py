5903cfff97258567748d9267c6a8c8d1c586263f
kosazna <kaznavouridis@gmail.com>
2021-04-08 18:41:05 +0300

    (D)

--- utilities/display.py
+++ utilities/display.py
@@ -33,7 +33,7 @@ class Display(metaclass=Singleton):
         self._content = []
 
     def __call__(self, content, kind=None):
-        if self._mode == 'CMD' or self._mode is None:
+        if self._mode == 'CLI' or self._mode is None:
             print(self._display(content, kind))
         else:
             self._content.append(self._display(content, kind))

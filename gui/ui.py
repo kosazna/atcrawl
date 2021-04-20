0d9acd7b8ead0e68475fce39ed8cc573e4b3d6ed
kosazna <kaznavouridis@gmail.com>
2021-04-20 21:37:10 +0300

    (D)

--- gui/ui.py
+++ gui/ui.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from PyQt5.QtGui import QIntValidator, QValidator
 from crawlers.rellasamortiser.crawler import RellasAmortiserBrand
 from PyQt5.QtWidgets import QFileDialog, QMainWindow
 from PyQt5.QtCore import QThreadPool

85a76755a9ec6197597fa5f66c004f35d7ae854d
kosazna <kaznavouridis@gmail.com>
2021-09-14 13:03:33 +0300

    (D)

--- settings.py
+++ settings.py
@@ -4,12 +4,13 @@ import os
 from pathlib import Path
 from atcrawl.utilities.funcs import load_user_settings
 
-DEBUG = True
+DEBUG = False
 
 USER_SETTINGS_FILE = Path.home().joinpath(".atcrawl\\settings.json")
 USER_SETTINGS = load_user_settings(USER_SETTINGS_FILE)
 
-BASE_DIR = os.path.abspath('.')
+# BASE_DIR = os.path.abspath('.')
+BASE_DIR = str(Path(__file__).parent)
 STATIC = "static"
 
 if not DEBUG:
